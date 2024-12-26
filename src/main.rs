d76c01aed970b68fe35c07c6222edd1ced42fbfd
Jason Stanley <jasonstanleyyoman@gmail.com>
2024-12-26 17:18:42 +0700

    remove unused import

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use minigrep::Config;
-use std::{env, error::Error, fs, process};
+use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
